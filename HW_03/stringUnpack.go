e208f81283d3b1a6bbd662fe8064bb5b7780dd5c
akaKAIN <mkain@mail.ru>
2020-01-26 18:56:11 +1000

    Решено ДЗ к третьему модулю

--- HW_03/stringUnpack.go
+++ HW_03/stringUnpack.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"strconv"
+)
+
 /*
 Домашнее задание
 Распаковка строки
@@ -11,8 +17,61 @@ package main
 * `qwe\4\5` => `qwe45` (*)
 * `qwe\45` => `qwe44444` (*)
 * `qwe\\5` => `qwe\\\\\` (*)
- */
+*/
 
 func main() {
+	var input string
 
+	fmt.Println("Inter row")
+	if _, err := fmt.Scan(&input); err != nil {
+		log.Fatal(err)
+	}
+
+	sepRow := separator(input)
+	result, err := multiplySymbol(sepRow)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(result)
+
+}
+
+func separator(row string) []string {
+	var tmp string
+	var result []string
+	for i := range row {
+		switch {
+		case row[i] >= 48 && row[i] <= 57:
+			tmp += string(row[i])
+		case row[i] > 57 && tmp != "":
+			result = append(result, tmp, string(row[i]))
+			tmp = ""
+		default:
+			result = append(result, string(row[i]))
+		}
+	}
+
+	if tmp != ""{
+		result = append(result, tmp)
+	}
+	return result
+}
+
+func multiplySymbol(strSlice []string) (string, error) {
+	var result string
+	for i := range strSlice{
+		num, err := strconv.Atoi(strSlice[i])
+		if err != nil {
+			result += strSlice[i]
+			continue
+		}
+		for j:=0; j<num-1; j++{
+			if i == 0{
+				err = fmt.Errorf("Некорректная строка")
+				return strSlice[i], err
+			}
+			result += strSlice[i-1]
+		}
+	}
+	return result, nil
 }
