80e431cc520a63b754f7a99ef855225b593a5fa9
akaKAIN <mkain@mail.ru>
2020-01-28 09:05:25 +1000

    Добавлена функция получения 5 последних ключей с максимальным значеним. Добавлена функция реверса слайса. Добавлены тесты.

--- HW_04/getTenWords.go
+++ HW_04/getTenWords.go
@@ -13,7 +13,7 @@ import (
 const textRow = "Когда вся эта ученая толпа успевала приходить несколько ранее или когда знали, что профессора будут позже обыкновенного, тогда, со всеобщего согласия, замышляли бой, и в этом бою должны были участвовать все, даже цензора, обязанные смотреть за порядком и нравственностию всего учащегося сословия. Два богослова обыкновенно решали, как происходить битве: каждый ли класс должен стоять за себя особенно или все должны разделиться на две половины: на бурсу и семинарию. Во всяком случае, грамматики начинали прежде всех, и как только вмешивались риторы, они уже бежали прочь и становились на возвышениях наблюдать битву."
 
 func main() {
-	result := GetMap(textRow)
+	result := GetTopKeys(GetMap(textRow))
 	fmt.Println(result)
 }
 
@@ -41,4 +41,33 @@ func GetMap(text string) map[string]int {
 		result[tmp]++
 	}
 	return result
+}
+
+func GetTopKeys(data map[string]int) []string {
+	var row = make([][]string, len(data))
+	var resultRow []string
+	var max int
+
+	for word, count := range data {
+		row[count-1] = append(row[count-1], word)
+	}
+	for i := range row {
+		resultRow = append(resultRow, row[i]...)
+	}
+	max = len(resultRow)
+	if max <= 5 {
+		return resultRow
+	} else {
+		return resultRow[max-5:]
+	}
+
+}
+
+func ReverseSlice(s []string) []string {
+	fmt.Println("S in:", s)
+	for i := 0; i < len(s)/2; i++ {
+		s[i], s[len(s)-i-1] = s[len(s)-i-1], s[i]
+	}
+	fmt.Println("S out:", s)
+	return s
 }
--- HW_04/getTenWords_test.go
+++ HW_04/getTenWords_test.go
@@ -1,24 +1,33 @@
 package main
 
 import (
+	"fmt"
+	"reflect"
 	"testing"
 )
 
-const TestText = "Один, два, ТрИ: дВа. Четыре, Три Четыре четыре трИ. ЧЕТЫРЕ"
+const TestText = "ШЕСТЬ Один, пять пять шесть ШесТЬ, два, ТрИ: дВа. Четыре, шесть Пять. ПЯТЬ: Шесть. Три Четыре ШЕстЬ. четыре пять трИ. ЧЕТЫРЕ"
 
-type Result struct {
+type ResultMap struct {
 	len int
 	m   map[string]int
 }
 
+type ResultSlice struct {
+	len int
+	s   []string
+}
+
 func TestGetMap(t *testing.T) {
-	var right = Result{
-		4,
+	var right = ResultMap{
+		6,
 		map[string]int{
 			"один":   1,
 			"два":    2,
 			"три":    3,
 			"четыре": 4,
+			"пять":   5,
+			"шесть":  6,
 		}}
 	result := GetMap(TestText)
 	if right.len != len(result) {
@@ -27,6 +36,32 @@ func TestGetMap(t *testing.T) {
 	for key, val := range result {
 		if right.m[key] != val {
 			t.Errorf("Ожидается: %v, получено: %v", right.m[key], val)
+		}
+	}
+}
+
+func TestGetTopKeys(t *testing.T) {
+	var rigth = ResultSlice{5, []string{"шесть", "пять", "четыре", "три", "два"}}
+	k := GetTopKeys(GetMap(TestText))
+	if rigth.len != len(k){
+		t.Errorf("Длина сайса неверна. Получено: %v  Ожидается: %v\n", len(k), rigth.len)
+	}
+	if reflect.DeepEqual(rigth.s, k){
+		t.Errorf("Ошибка содержимого слайса.\nПолучено: %v\nОжидается: %v\n", k, rigth.s)
+	}
+	fmt.Printf("Ожидается:%v\nПолучено:%v\n", rigth.s, k)
+}
+
+func TestReverseSlice(t *testing.T) {
+
+	var right = []string{"5","4","3","2","1"}
+	s := ReverseSlice([]string{"1","2","3","4","5"})
+	if len(right) != len(s) {
+		t.Errorf("Длина сайса неверна. Получено: %v  Ожидается: %v\n", len(s), len(right))
+	}
+	for i:=0; i<len(right); i++{
+		if right[i] != s[i]{
+			t.Errorf("Ошибка содержимогo. Получено: %v  Ожидается: %v\n", s[i], right[i])
 		}
 	}
 }
