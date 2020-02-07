ed2a743518880a3b97e6844b1af560c67f1ddcbe
akaKAIN <mkain@mail.ru>
2020-02-08 09:19:20 +1000

    Добавлено тестирование функции удаления файлов

--- HW_05/alterCron_test.go
+++ HW_05/alterCron_test.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
 	"reflect"
+	"strconv"
 	"strings"
 	"testing"
+	"time"
 )
 
 func TestGetOrCreateDir(t *testing.T) {
@@ -15,7 +18,7 @@ func TestGetOrCreateDir(t *testing.T) {
 	dir, imageDir := right[0], right[1]
 
 	absPath, err := filepath.Abs(dir)
-	if err !=nil{
+	if err != nil {
 		log.Fatal(err)
 	}
 
@@ -23,18 +26,32 @@ func TestGetOrCreateDir(t *testing.T) {
 
 	r := GetOrCreateDir(DirTo, ImageDir)
 
-	if f, err := os.Stat(fullPath); os.IsExist(err){
+	if f, err := os.Stat(fullPath); os.IsExist(err) {
 		t.Fatalf("fileInfo: %v\nerrTest: %v\n", f, err)
 	}
 
-
 	testingSlice := strings.Split(r, "/")
 	s := testingSlice[len(testingSlice)-2:]
-	if !reflect.DeepEqual(right, s){
+	if !reflect.DeepEqual(right, s) {
 		t.Fatalf("Ожидается: %v\nПолучено: %v", right, s)
 	}
 }
 
 func TestDelFile(t *testing.T) {
+	fileName := strconv.Itoa(int(time.Now().UnixNano()))
+	var buf = []byte("Create for test, delete file if u find it")
 
+	if err := ioutil.WriteFile(fileName, buf, os.ModePerm); err != nil {
+		t.Fatal("Ошибка создания тестового файла.")
+	}
+	TestFile, err := os.Stat(fileName)
+	if err != nil || TestFile.IsDir() {
+		t.Fatal("После тестового создания, файл не обнаружен")
+	}
+	if err := DelFile(TestFile, "."); err != nil {
+		t.Fatalf("Ошибка удаления файла: %s", TestFile.Name())
+	}
+	if _, err := os.Stat(TestFile.Name()); err == nil{
+		t.Fatalf("Файл %q не был удален в ходе теста", TestFile.Name())
+	}
 }
