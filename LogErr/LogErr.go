7ceeb2eec279d4e400c607b71f4e5d547fabd05c
akaKAIN <mkain@mail.ru>
2020-02-24 04:58:17 +1000

    Реализовано логирование.

--- LogErr/LogErr.go
+++ LogErr/LogErr.go
@@ -8,11 +8,11 @@ import (
 )
 
 var (
-	inFile, _ = os.OpenFile("error.log", os.O_CREATE|os.O_APPEND, os.ModePerm)
-	LogThis = log.New(inFile, fmt.Sprint(time.Now().Format("02.01.2006 15:04:05 ")), 0)
+	inFile, _ = os.OpenFile("error.log", os.O_APPEND|os.O_RDWR|os.O_CREATE, os.ModePerm)
+	LogThis   = log.New(inFile, fmt.Sprint(time.Now().Format("02.01.2006 15:04:05 ")), 0)
 )
 
-func CheckErr(err error){
+func CheckErr(err error) {
 	if err != nil {
 		LogThis.Println(err)
 	}
